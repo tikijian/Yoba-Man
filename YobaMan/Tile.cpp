0e99fde33d4b22beac29246c7c957ea67372f833
Tikijian <timurt1988@gmail.com>
2017-10-01 23:54:30 +0300

    started work on Tile class

--- YobaMan/Tile.cpp
+++ YobaMan/Tile.cpp
@@ -1,12 +1,36 @@
 #include "stdafx.h"
 #include "Tile.h"
 
-
 Tile::Tile()
 {
+	walls.emplace("north", false);
+	walls.emplace("east", false);
+	walls.emplace("south", false);
+	walls.emplace("west", false);
+
+	initialize();
+}
+
+Tile::Tile(bool north, bool east, bool south, bool west)
+{
+	walls.emplace("north", north);
+	walls.emplace("east", east);
+	walls.emplace("south", south);
+	walls.emplace("west", west);
+
+	initialize();
+}
+
+bool Tile::hasWallAt(const string side)
+{
+	return walls.at(side) ? true : false;
 }
 
 
 Tile::~Tile()
+{
+}
+
+void Tile::draw(RenderTarget & target, RenderStates states) const
 {
 }
--- YobaMan/Tile.h
+++ YobaMan/Tile.h
@@ -1,9 +1,24 @@
 #pragma once
-class Tile
+#include "SFML\Graphics.hpp"
+#include <unordered_map>
+#include <string>
+
+using namespace sf;
+using namespace std;
+
+class Tile : public Drawable
 {
+	void initialize();
 public:
 	static const int SIZE = 64;
+	unordered_map<string, bool> walls;
+
 	Tile();
+	Tile(bool north, bool east, bool south, bool west);
+
+	bool hasWallAt(const string);
+
+	virtual void draw(RenderTarget & target, RenderStates states) const override;
 	~Tile();
 };
 
